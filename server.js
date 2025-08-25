1a62fe4987be0c72f4dbabc8289db1a0c1f6a730
RajYadav71 <yjagdish1983@gmail.com>
2025-08-25 10:44:26 +0530

    1

--- server.js
+++ server.js
@@ -26,3 +26,46 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model("User", userSchema);
+
+
+
+app.post("/user", async (req, res) => {
+try{
+    const { name, email,phone, password } = req.body; 
+
+    const user = new User({ name, email,phone, password });
+    await user.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } 
+  catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/user/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+
+    if (user.password !== password) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    console.error("Error logging in user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port http://localhost:3000");
+});
+
